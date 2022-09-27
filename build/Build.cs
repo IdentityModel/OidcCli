eb498d7579ad29b246c4244a6e40d1e58eb72199
Eric Sondergard <4650644+esond@users.noreply.github.com>
2022-09-27 12:55:22 -0600

    Setup nuke build to work with GitHub Actions

--- build/Build.cs
+++ build/Build.cs
@@ -1,16 +1,21 @@
-using System;
-using System.Linq;
 using Nuke.Common;
-using Nuke.Common.CI;
-using Nuke.Common.Execution;
+using Nuke.Common.CI.GitHubActions;
 using Nuke.Common.IO;
 using Nuke.Common.ProjectModel;
-using Nuke.Common.Tooling;
-using Nuke.Common.Utilities.Collections;
-using static Nuke.Common.EnvironmentInfo;
+using Nuke.Common.Tools.DotNet;
 using static Nuke.Common.IO.FileSystemTasks;
-using static Nuke.Common.IO.PathConstruction;
+using static Nuke.Common.Tools.DotNet.DotNetTasks;
 
+[GitHubActions(
+    "continuous",
+    GitHubActionsImage.WindowsLatest,
+    GitHubActionsImage.UbuntuLatest,
+    GitHubActionsImage.MacOsLatest,
+    OnPushBranchesIgnore = new[] { MainBranch, $"{ReleaseBranchPrefix}/*" },
+    OnPullRequestBranches = new[] { MainBranch },
+    InvokedTargets = new[] { nameof(Compile) },
+    CacheKeyFiles = new[] { "global.json", "source/**/*.csproj" },
+    EnableGitHubToken = true)]
 class Build : NukeBuild
 {
     /// Support plugins are available for:
@@ -24,21 +29,38 @@ class Build : NukeBuild
     [Parameter("Configuration to build - Default is 'Debug' (local) or 'Release' (server)")]
     readonly Configuration Configuration = IsLocalBuild ? Configuration.Debug : Configuration.Release;
 
+    [Solution] readonly Solution Solution;
+
+    AbsolutePath SourceDirectory => RootDirectory / "src";
+    AbsolutePath OutputDirectory => RootDirectory / "output";
+
+    const string MainBranch = "main";
+    const string ReleaseBranchPrefix = "release";
+
     Target Clean => _ => _
         .Before(Restore)
         .Executes(() =>
         {
+            DotNetClean(s => s
+                .SetProject(Solution));
+
+            EnsureCleanDirectory(OutputDirectory);
         });
 
     Target Restore => _ => _
         .Executes(() =>
         {
+            DotNetRestore(s => s
+                .SetProjectFile(Solution));
         });
 
     Target Compile => _ => _
-        .DependsOn(Restore)
+        .DependsOn(Clean, Restore)
         .Executes(() =>
         {
+            DotNetBuild(s => s
+                .SetProjectFile(Solution)
+                .SetConfiguration(Configuration)
+                .EnableNoRestore());
         });
-
 }
